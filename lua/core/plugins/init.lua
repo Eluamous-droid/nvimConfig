1349c8853075c993bfa233fe4ad6963d2f8a6467
Mikkel Hansen <themik1337@gmail.com>
2023-10-18 17:02:35 +0200

    Split init.lua into multiple files for different plugins

--- lua/core/plugins/init.lua
+++ lua/core/plugins/init.lua
@@ -2,7 +2,8 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {
-  { 'mfussenegger/nvim-jdtls', lazy = false },
-
-}
+require ("core.plugins.harpoon")
+require ("core.plugins.litee")
+require("core.plugins.lsp")
+require("core.plugins.telescope")
+require("core.plugins.treesitter")
