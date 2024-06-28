b5ebd5b832173e61e8520a6b803d3bd170cbdb78
Mikkel Hansen <themik1337@gmail.com>
2024-06-28 14:30:29 +0200

    Fixed java lsp not recognising files, moved all jdtls to self managed folders, missing debug

--- ftplugin/java.lua
+++ ftplugin/java.lua
@@ -3,7 +3,7 @@ local project_name = vim.fn.fnamemodify(vim.fn.getcwd(), ':p:h:t')
 
 local api = vim.api
 local jdtls = require('jdtls')
-local root_markers = { 'gradlew', '.git' }
+local root_markers = { 'gradlew', '.git', 'pom.xml', '.jazz5', '.project' }
 local root_dir = require('jdtls.setup').find_root(root_markers)
 local home = os.getenv('HOME')
 
@@ -17,7 +17,8 @@ local config = {
   cmd = {
 
     -- 💀
-    '/opt/homebrew/Cellar/openjdk@17/17.0.11/bin/java', -- or '/path/to/java17_or_newer/bin/java'
+    '/Users/dk000258/.vscode/extensions/redhat.java-1.31.0-darwin-arm64/jre/17.0.11-macosx-aarch64/bin/java', -- or '/path/to/java17_or_newer/bin/java'
+    --'/Library/Java/JavaVirtualMachines/openjdk-21.jdk/Contents/Home/bin/java', -- or '/path/to/java17_or_newer/bin/java'
     -- depends on if `java` is in your $PATH env variable and if it points to the right version.
 
     '-Declipse.application=org.eclipse.jdt.ls.core.id1',
@@ -26,19 +27,22 @@ local config = {
     '-Dlog.protocol=true',
     '-Dlog.level=ALL',
     '-Xmx1g',
+		--'-javaagent:/Users/dk000258/.vscode/extensions/redhat.java-1.31.0-darwin-arm64/lombok/lombok-1.18.32.jar',
     '--add-modules=ALL-SYSTEM',
     '--add-opens', 'java.base/java.util=ALL-UNNAMED',
     '--add-opens', 'java.base/java.lang=ALL-UNNAMED',
 
     -- 💀
-    '-jar', '/opt/homebrew/Cellar/jdtls/1.35.0/libexec/plugins/org.eclipse.equinox.launcher_1.6.800.v20240330-1250.jar',
+
+		'-jar', '/Users/dk000258/Documents/ForFun/jdtlsStuff/jdt-language-server-1.28.0-202309281329/plugins/org.eclipse.equinox.launcher_1.6.500.v20230717-2134.jar',
     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^                                       ^^^^^^^^^^^^^^
     -- Must point to the                                                     Change this to
     -- eclipse.jdt.ls installation                                           the actual version
 
 
     -- 💀
-    '-configuration', '/opt/homebrew/Cellar/jdtls/1.35.0/libexec/config_mac_arm/',
+    '-configuration', '/Users/dk000258/Documents/ForFun/jdtlsStuff/jdt-language-server-1.28.0-202309281329/config_mac_arm/',
+
     -- ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^        ^^^^^^
     -- Must point to the                      Change to one of `linux`, `win` or `mac`
     -- eclipse.jdt.ls installation            Depending on your system.
@@ -52,14 +56,13 @@ local config = {
   -- 💀
   -- This is the default if not provided, you can remove it. Or adjust as needed.
   -- One dedicated LSP server & client will be started per unique root_dir
-  root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew' }),
+  root_dir = require('jdtls.setup').find_root({ '.git', 'mvnw', 'gradlew', 'pom.xml'}),
 
   -- Here you can configure eclipse.jdt.ls specific settings
   -- See https://github.com/eclipse/eclipse.jdt.ls/wiki/Running-the-JAVA-LS-server-from-the-command-line#initialize-request
   -- for a list of options
   settings = {
-    java = {
-      autobuild = { enabled = true },
+	    autobuild = { enabled = true },
       maxConcurrentBuilds = 8,
       signatureHelp = { enabled = true },
       contentProvider = { preferred = 'fernflower' },
@@ -103,16 +106,12 @@ local config = {
           useJava7Objects = true,
         },
         useBlocks = true,
-      },
+      },    java = {
       configuration = {
         runtimes = {
           {
             name = "JavaSE-1.8",
             path = "/Library/Java/JavaVirtualMachines/jdk1.8.0_311.jdk/Contents/Home/jre/",
-          },
-          {
-            name = "JavaSE-17",
-            path = "/opt/homebrew/Cellar/openjdk@17/17.0.11/",
           },
         }
       },
@@ -135,13 +134,13 @@ local config = {
 -- This bundles definition is the same as in the previous section (java-debug installation)
 local bundles = {
   vim.fn.glob(
-    "/Users/dk000258/Documents/ForFun/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
+   "/Users/dk000258/Documents/ForFun/jdtlsStuff/java-debug/com.microsoft.java.debug.plugin/target/com.microsoft.java.debug.plugin-*.jar",
     1),
 };
 
 -- This is the new part
 vim.list_extend(bundles,
-  vim.split(vim.fn.glob("/Users/dk000258/Documents/ForFun/vscode-java-test/server/*.jar", 1), "\n"))
+  vim.split(vim.fn.glob("/Users/dk000258/Documents/ForFun/jdtlsStuff/vscode-java-test/server/*.jar", 1), "\n"))
 config['init_options'] = {
   bundles = bundles,
 }
