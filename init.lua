07b90e368425b960a4fd545a65d81213cff73787
Mikkel Hansen <themik1337@gmail.com>
2025-10-14 11:41:10 +0200

    Added obsidian.nvim

--- init.lua
+++ init.lua
@@ -268,7 +268,32 @@ require('lazy').setup({
         'stevearc/dressing.nvim', -- optional for vim.ui.select
     },
     config = true,
-}
+  },
+  {
+    "obsidian-nvim/obsidian.nvim",
+    version = "*", -- recommended, use latest release instead of latest commit
+    ft = "markdown",
+    -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+    -- event = {
+    --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+    --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+    --   -- refer to `:h file-pattern` for more examples
+    --   "BufReadPre path/to/my-vault/*.md",
+    --   "BufNewFile path/to/my-vault/*.md",
+    -- },
+    ---@module 'obsidian'
+    ---@type obsidian.config
+    opts = {
+      workspaces = {
+        {
+          name = "TheForbiddenOne",
+          path = "~/Documents/ObsidianNotes/TheForbiddenOne/",
+        },
+      },
+
+      -- see below for full list of options 👇
+    }
+  }
 }, {})
 require("core.options.options")
 require("core.options.remap")
--- lua/core/plugins/snacks.lua
+++ lua/core/plugins/snacks.lua
@@ -33,7 +33,7 @@ return {  "folke/snacks.nvim",
 		{ "<leader>,", function() Snacks.picker.buffers() end, desc = "Buffers" },
 		{ "<leader>:", function() Snacks.picker.command_history() end, desc = "Command History" },
 		{ "<leader>n", function() Snacks.picker.notifications() end, desc = "Notification History" },
-		-- { "<leader>e", function() Snacks.explorer() end, desc = "File Explorer" },
+		{ "<leader>eq", function() Snacks.explorer() end, desc = "File Explorer" },
 		-- find
 		{ "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
 		{ "<leader>fc", function() Snacks.picker.files({ cwd = vim.fn.stdpath("config") }) end, desc = "Find Config File" },
